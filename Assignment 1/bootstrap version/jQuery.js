c5b43e2e9fd9d366f290fc073180a370ae90123d
jaberfarah <jabir.farah@gmail.com>
2016-11-03 19:27:54 +0200

    added section 2 corresponding code
    updated the css to prevent classes duplications

--- Assignment 1/bootstrap version/jQuery.js
+++ Assignment 1/bootstrap version/jQuery.js
@@ -1,10 +1,31 @@
 $(document).ready(function() {
-    $( '.dropDown' ).hover(
+    $( '.drop1' ).hover(
         function(){
-            $(this).children('.subMenu1').slideDown(100);
+            $(this).children('#sub1').slideDown(100);
         },
         function(){
-            $(this).children('.subMenu1').slideUp(100);
+            $(this).children('#sub1').slideUp(100);
         }
     );
+
+    /* section 2 slide down code */
+
+
+
+
+    $('.drop2').click(
+    	function(){
+    		$(this).children('#sub2').slideDown(100);
+    	}
+    );
+    $(document).on("click",
+    	function(){
+    		$('#sub2').slideUp(100);
+    	}
+    )
+    $('.drop2').click(
+    	function(event){
+    		event.stopPropagation();
+    	}
+    )
 });
